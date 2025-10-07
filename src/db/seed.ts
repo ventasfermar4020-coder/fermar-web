33ce41fc7642b38b55d532d8aa1c79b8215ce207
Alonso-pixel <kreitos_05@outlook.com>
2025-10-07 12:39:17 -0600

    added price id

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -15,8 +15,8 @@ async function main() {
         name: "Playmobil City Life",
         description:
           "Juguete de construcción de la serie City Life de Playmobil, ideal para niños a partir de 4 años.",
-        price: "699.00",
-        stripePriceId: null, // Add your Stripe Price ID here (e.g., "price_1ABC123...")
+        price: "299.00",
+        stripePriceId: "price_1SFfBLCXE9gLIJzj2CTp4U8e", // Add your Stripe Price ID here (e.g., "price_1ABC123...")
         image: "/products/playmobil.png",
         stock: 25,
         isActive: true,
